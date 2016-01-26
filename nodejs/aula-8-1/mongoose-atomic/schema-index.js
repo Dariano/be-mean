0545121f94f6035dc1e2841abcb1d3e542ead99c
dariano <darianosoares@hotmail.com>
2016-01-26 00:18:42 -0200

    Criando index usando mongoose

--- nodejs/aula-8-1/mongoose-atomic/schema-index.js
+++ nodejs/aula-8-1/mongoose-atomic/schema-index.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.connect('mongodb://localhost/test')
+
 var userSchema = new Schema({
 	name: String,
 	email: String,
-	create_at: { type: String, default: Date.now, index: true }
+	create_at: {
+		type: String,
+		default: Date.now,
+		index: true
+	}
 });
 
-userSchema.index({ name:1, type: -1});
+const User = mongoose.model('Usuario', userSchema);
+const _user = {
+	name: 'dariele soares',
+	email: 'dariele@hotmail.com'
+};
+
+userSchema.index({
+	name: 1,
+	email: -1
+});
+
+User.create(_user, (err, data) => {
+	if (err) throw err;
+
+	console.log('Inseriu:', data);
+
+	process.exit(1);
+});
